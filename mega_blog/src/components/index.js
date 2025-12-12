b85e1ed125a388b0d4a7024ab23ac8f9f8c3658f
yashdubey2004 <dubeyyash310@gmail.com>
2025-12-12 14:26:30 +0530

    pages made

--- mega_blog/src/components/index.js
+++ mega_blog/src/components/index.js
@@ -1,9 +1,30 @@
-import Header from './Header';
-import Footer from './Footer';
-import container from './container/container';
-
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./Authlayout";
+import Input from "./Input";
 export {
     Header,
     Footer,
-    container   
-};
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
